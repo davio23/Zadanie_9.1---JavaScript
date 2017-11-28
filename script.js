ab6a5602d3bfe1dcf04ceb562a3ac7ef026fed80
davio23 <33763332+davio23@users.noreply.github.com>
2017-11-28 18:17:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-var a=6;
-var h=7; 
-var triangleArea=a*h/2; 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var a= 10;
+var h= 6;
+function getTriangleArea(a, h) {
+if (a > 0 && h > 0) {
+return a*h/2;
+}
+else {
+console.log("Nieprawidłowe dane");
+}
+}
+var triangleArea = getTriangleArea(10,6);
+var triangle1Area = getTriangleArea(10,12);
+var triangle2Area = getTriangleArea(4,6);
+var triangle3Area = getTriangleArea(7,9);
+console.log(triangleArea);
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
