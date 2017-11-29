b1c92b7e359e53e85e98f8a784e9e747d5da8565
davio23 <33763332+davio23@users.noreply.github.com>
2017-11-29 17:03:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var a= 10;
-var h= 6;
+var podstawa = 10,
+    wysokość = 6;
 function getTriangleArea(a, h) {
-if (a > 0 && h > 0) {
-return a*h/2;
+  if (a > 0 && h > 0) {
+  return a * h / 2;
+  }
+  else {
+  console.log("Nieprawidłowe dane");
+  }
 }
-else {
-console.log("Nieprawidłowe dane");
-}
-}
-var triangleArea = getTriangleArea(10,6);
-var triangle1Area = getTriangleArea(10,12);
-var triangle2Area = getTriangleArea(4,6);
-var triangle3Area = getTriangleArea(7,9);
+var triangleArea = getTriangleArea(10,6),
+    triangle1Area = getTriangleArea(10,12),
+    triangle2Area = getTriangleArea(4,6),
+    triangle3Area = getTriangleArea(7,9);
 console.log(triangleArea);
 console.log(triangle1Area);
 console.log(triangle2Area);
